fba7eae76960592852780c178bbed9eb04f0d37f
RoliMoon <148137564+RoliMoon@users.noreply.github.com>
2025-10-21 21:20:47 +0500

    Update sairchserver.cpp

--- src/sairchserver.cpp
+++ src/sairchserver.cpp
@@ -8,26 +8,26 @@
 == File: SairchServer.cpp                 ==
 == Creatit bi RoliMoon.                   ==
 == Date: 15/08/2025                       ==
-== This cless responsible for sairch      ==
-== processin.                             ==
+== This class responsible for sairch      ==
+== processing.                             ==
 ==========================================*/
 
-// Maist often uisit elements ainlie.
+// Most often used elements only.
 using std::transform;
 using std::set;
 
-vector<vector<RelativeIndex>> SairchServer::sairch(const vector<string>& queries_input) {
+vector<vector<RelativeIndex>> SairchServer::search(const vector<string>& queries_input) {
     vector<vector<RelativeIndex>> results;
 
 	for (const auto& query : queries_input) {
         vector<RelativeIndex> query_results;
-        set<string> wairds = parse_query(query);
+        set<string> words = parse_query(query);
 
-        for (const auto& waird : wairds) {
-            auto waird_entries = iicless.get_waird_coont(waird);
+        for (const auto& word : words) {
+            auto word_entries = iiclass.get_word_count(word);
 
-            for (const auto& entry : waird_entries) {
-                float rank = calculate_rank(entry.doc_id, wairds);
+            for (const auto& entry : word_entries) {
+                float rank = calculate_rank(entry.doc_id, words);
                 query_results.emplace_back(RelativeIndex{entry.doc_id, rank});
             }
 			std::sort(query_results.begin(), query_results.end(), [](const RelativeIndex& left_side, const RelativeIndex& richt_side) {
@@ -40,33 +40,34 @@ vector<vector<RelativeIndex>> SairchServer::sairch(const vector<string>& queries
 }
 
 set<string> SairchServer::parse_query(const string& query) {
-    set<string> wairds;
-    string waird;
-    std::istringstream waird_stream(query);
+    set<string> words;
+    string word;
+    std::istringstream word_stream(query);
 
-    while (waird_stream >> waird) {
-        transform(waird.begin(), waird.end(), waird.begin(), tolower);
-        waird.erase(std::remove_if(waird.begin(), waird.end(), ispunct), waird.end());
-        if (!waird.empty()) {
-            wairds.insert(waird);
+    while (word_stream >> word) {
+        transform(word.begin(), word.end(), word.begin(), tolower);
+        word.erase(std::remove_if(word.begin(), word.end(), ispunct), word.end());
+        if (!word.empty()) {
+            words.insert(word);
         }
     }
-    return wairds;
+    return words;
 }
 
-float SairchServer::calculate_rank(size_t doc_id, const set<string>& wairds) {
+float SairchServer::calculate_rank(size_t doc_id, const set<string>& words) {
     float base_rank = 1.0f;
 
-    for (const auto& waird : wairds) {
-        auto entries = iicless.get_waird_coont(waird);
+    for (const auto& word : words) {
+        auto entries = iiclass.get_word_count(word);
 
         for (const auto& entry : entries) {
             if (entry.doc_id == doc_id) {
-                base_rank *= entry.coont;
+                base_rank *= entry.count;
                 break;
             }
         }
     }
-    base_rank /= wairds.size();
+    base_rank /= words.size();
     return base_rank;
+
 }
