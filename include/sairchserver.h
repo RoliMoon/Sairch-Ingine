2a5a600bbc2fea59fcbd2dd41b250374275508bb
RoliMoon <148137564+RoliMoon@users.noreply.github.com>
2025-11-14 22:03:07 +0500

    Update sairchserver.h

--- include/sairchserver.h
+++ include/sairchserver.h
@@ -30,12 +30,12 @@ struct RelativeIndex {
 
 class SearchServer {
 public:
-    SearchServer(InvertitIndex& idx) : iiclass(idx){ };
+    SearchServer(InvertedIndex& idx) : iiclass(idx){ };
     vector<vector<RelativeIndex>> search(const vector<string>& queries_input);
     set<string> parse_query(const string& query);
     float calculate_rank(size_t doc_id, const set<string>& words);
 private:
     //RelativeIndex relative_index;
-    InvertitIndex iiclass;
+    InvertedIndex iiclass;
 
 };
