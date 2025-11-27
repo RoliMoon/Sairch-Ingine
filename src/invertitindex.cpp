d139fe048233ac48c44ae1dd528f6f3fe217070f
RoliMoon <148137564+RoliMoon@users.noreply.github.com>
2025-11-27 21:19:02 +0500

    Update invertitindex.cpp

--- src/invertitindex.cpp
+++ src/invertitindex.cpp
@@ -17,6 +17,7 @@
 // Most often used elements only.
 using std::transform;
 using std::thread;
+using std::map;
 
 void InvertedIndex::update_document_base(vector<string>& input_docs) {
     /*if (!input_docs.empty()) {
@@ -24,19 +25,21 @@ void InvertedIndex::update_document_base(vector<string>& input_docs) {
         freq_dictionary.clear();
         docs.reserve(input_docs.size());
     }*/
+    freq_dictionary.clear();
 
     vector<thread> thread_pool;
-    
     size_t num_threads = thread::hardware_concurrency();
     if (num_threads == 0) num_threads = 1;
 
+  //Vector o frequency dictionars for single documents.
+    vector<map<string, vector<Entry>>> all_perdoc_freq_dictionaries(num_threads); 
     size_t docs_per_thread = input_docs.size() / num_threads;
 
     //for (size_t doc_id = 0; doc_id < input_docs.size(); doc_id++) {
         //docs.push_back(input_docs[doc_id]);
 
-    auto process_range = [&](size_t doc_id_begin, size_t doc_id_final) {
-        std::map<string, vector<Entry>> perdoc_freq_dictionary;
+    auto process_range = [&](size_t thread_id, size_t doc_id_begin, size_t doc_id_final) {
+        map<string, vector<Entry>> perdoc_freq_dictionary;
 
         for (size_t doc_id = doc_id_begin; doc_id < doc_id_final;  doc_id++) {
           // Separating documents until single words.
@@ -57,29 +60,33 @@ void InvertedIndex::update_document_base(vector<string>& input_docs) {
                 }
             }
         }
-        std::lock_guard<std::mutex> lock(dictionar_mutex);
-        for (const auto& dict_pair : perdoc_freq_dictionary) {
-            const string& word = dict_pair.first;
-            const vector<Entry>& entries = dict_pair.second;
-
-            freq_dictionary[word].insert(freq_dictionary[word].end(), entries.begin(), entries.end());
-        }
+        all_perdoc_freq_dictionaries[thread_id] = std::move(perdoc_freq_dictionary);
     };
     
   // Launchin threads
     for (size_t i = 0; i < num_threads; i++) {
         size_t begin = i * docs_per_thread;
-        size_t final = (i == num_threads - 1) ? input_docs.size() : (i + 1 * docs_per_thread);
+        size_t final = (i == num_threads - 1) ? input_docs.size() : ((i + 1) * docs_per_thread);
         if (begin < final) {
-            thread_pool.emplace_back(process_range, begin, final);
+            thread_pool.emplace_back(process_range, i, begin, final);
         }
     }
-  // Closing threads
+
+    // Closing threads
     for (auto& thread : thread_pool) {
         if (thread.joinable()) {
             thread.join();
         }
     }
+
+    for (const auto& local_dictionary : all_perdoc_freq_dictionaries) {
+        for (const auto& dict_pair : local_dictionary) {
+            const string& word = dict_pair.first;
+            const vector<Entry>& entries = dict_pair.second;
+            freq_dictionary[word].insert(freq_dictionary[word].end(), entries.begin(), entries.end());
+        }
+    }
+  
     docs = std::move(input_docs);
     //docs = input_docs;
 }
